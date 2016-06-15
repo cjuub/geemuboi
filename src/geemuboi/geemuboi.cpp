c0abcbbc9eaa2ae7cbb5e5bde77cf9e44039ecb7
cjuub <cjuuub@gmail.com>
2016-06-15 12:14:23 +0200

    Implement FPS counter

--- src/geemuboi/geemuboi.cpp
+++ src/geemuboi/geemuboi.cpp
@@ -5,8 +5,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
 
 #include <SDL2/SDL.h>
+
+const unsigned CYCLES_PER_FRAME = 70224;
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -20,12 +23,25 @@ int main(int argc, char* argv[]) {
     GPU gpu;
     MMU mmu(gpu, bios, rom);
     CPU cpu(mmu);
-
     SDL_Event event;
+
+    time_t start_time = time(0);
+    unsigned frames = 0;
     bool run = true;
     while (run) {
-        int cycles = cpu.execute();
-        gpu.step(cycles);
+        int frame_cycles = 0;
+        while (frame_cycles <= CYCLES_PER_FRAME) {
+            int cycles = cpu.execute();
+            gpu.step(cycles);
+            frame_cycles += cycles;
+        }
+
+        ++frames;
+        if (difftime(time(0), start_time) >= 1) {
+            start_time = time(0);
+            std::cout << std::dec << frames << std::endl;
+            frames = 0;
+        }
 
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
@@ -34,6 +50,5 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // SDL_DestroyWindow( window );
     SDL_Quit();
 }
